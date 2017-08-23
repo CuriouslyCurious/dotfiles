f2f283c9a9b622b9f219c41dc962952ee705c1ad
CuriouslyCurious <thealmightywarrior@gmail.com>
2017-08-23 15:18:55 +0200

    Changed polybar look

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar top &
-polybar top-right &
+#polybar top &
+#polybar top-right &
+
+polybar main &
 
 echo "Bars launched..."
