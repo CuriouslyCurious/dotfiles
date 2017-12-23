29519ebf025e662fc985dc66d894996bc614f6ed
CuriouslyCurious <ojaloe-5@student.ltu.se>
2017-12-23 09:33:48 +0100

    Removed fonts folder

--- nvim/.vim/templates/skeleton.py
+++ nvim/.vim/templates/skeleton.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
 if __name__ == "__main__":
--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,7 +9,7 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 #polybar top &
 #polybar top-right &
 
-MONITOR=DP-1 polybar main &
+MONITOR=LVDS1 polybar main &
 MONITOR=HDMI-1 polybar secondary &
 MONITOR=DVI-D-1 polybar tertiary &
 
