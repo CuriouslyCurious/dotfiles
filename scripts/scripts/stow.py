850644f3be60bfb9f887c0c43f29a0c9b0e0a286
CuriouslyCurious <ojaloe-5@student.ltu.se>
2017-12-28 17:21:20 +0100

    stow.py: Functioning now

--- scripts/scripts/stow.py
+++ scripts/scripts/stow.py
@@ -2,20 +2,24 @@
 # -*- coding: utf8 -*-
 
 """
-An implementation of stow in Python
+An implementation of stow in Python.
 
 This program assumes that the dotfiles folder is located at
-$HOME/dotfiles
+$HOME/dotfiles.
 
-TODO: Ensure it properly respects non-empty directories
+It will by default create symlinked folders instead of symlinking just files.
+
+TODO:
+* Make an option to disable symlinked folder creation
 """
 
 __author__ = "curious"
 
 import argparse
+import os
 import pathlib
-import textwrap
 import shutil
+import textwrap
 
 from sys import exit
 
@@ -29,91 +33,142 @@ parser = argparse.ArgumentParser(
 
                 WARNING: This script may crush your dreams (and files) if you are
                 not careful. Read the prompts carefully."""))
-parser.add_argument("-s", "--skip", dest="skip", action="store_true", default=False,
-                    help="skip any conflicts")
-parser.add_argument("-N", "--NO", dest="no", action="store_true", default=False,
-                    help="don't perform any symlinks, just print the results.")
-parser.add_argument("-Y", "--YES", dest="yes", action="store_true", default=False,
-                    help="say yes to all prompts")
-parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False,
-                    help="verbose mode")
-parser.add_argument("-r", "--remove", dest="remove", action="store_true", default=False,
-                    help="remove all symlinks")
-parser.add_argument("-R", "--replace", dest="replace", action="store_true", default=False,
-                    help="replace all existing files (no prompt)")
+
+# parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", default=False,
+#                     help="verbose mode")
+
+confirm = parser.add_mutually_exclusive_group()
+confirm.add_argument("-s", "--skip", dest="skip", action="store_true", default=False,
+                     help="skip any conflicts")
+confirm.add_argument("-N", "--NO", dest="no", action="store_true", default=False,
+                     help="don't perform any actions, just print the results.")
+confirm.add_argument("-Y", "--YES", dest="yes", action="store_true", default=False,
+                     help="say yes to all prompts")
+
+modes = parser.add_mutually_exclusive_group()
+modes.add_argument("-r", "--remove", dest="remove", action="store_true", default=False,
+                   help="remove all existing files (you will be prompted)")
+modes.add_argument("-R", "--replace", dest="replace", action="store_true", default=False,
+                   help="replace all existing files (you will be prompted)")
 args = parser.parse_args()
 
-if args.yes:
+if args.yes or args.no:
     args.verbose = True
 
 
-def sub_path(path, target):
-    for sub in path.iterdir():
-        if str(sub).split("/")[-1] == ".config":
-            for sub_sub in sub.iterdir():
-                target = str(target) + "/.config"
-                symlink(sub_sub, pathlib.Path(target))
-        else:
-            symlink(sub, target)
+class Colour:
+    RED = "\033[1;31m"
+    GREEN = "\033[0;32m"
+    YELLOW = "\033[0;33m"
+    BLUE = "\033[1;34m"
+    CYAN = "\033[1;36m"
+    RESET = "\033[0;0m"
+    BOLD = "\033[;1m"
+    REVERSE = "\033[;7m"
 
 
 def symlink(origin, target):
-    if len(target.parts) > 2:
-        target = target / origin.stem
-
-    if args.yes or args.verbose:
-        print_ln(origin, target)
+    if args.no:
         return
 
-    if not args.remove and (args.yes or prompt(origin, target)):
-        if not target.is_symlink():
-            if not target.is_file() and not target.is_dir():
-                target.symlink_to(origin, origin.is_dir())
-            else:
-                if args.skip or args.no:
-                    return
+    if args.yes:  # or args.verbose:
+        print_ln(origin, target)
 
-                if str(target) == "/etc":
-                    print("Skipping... /etc will never be replaced by this script.")
-                    return
-
-                if args.replace or prompt(origin, target, "replace"):
-                    if target.is_dir():
-                        shutil.rmtree(target)
-                    elif target.is_file():
-                        target.unlink()
-
-                    target.symlink_to(origin, origin.is_dir())
-
-    elif args.remove and (args.yes or prompt(origin, target, "remove")):
-        if str(target) == "/etc":
-            print("Skipping... /etc will never be replaced by this script.")
+    if args.remove:
+        if pathlib.Path.home() != pathlib.Path(*target.parts[:3]):
+            print(warning_colour("Skipping '%s'. It is outside of home folder." %
+                                 str(target)))
             return
 
-        if target.is_symlink():
-            target.unlink()
+        if target.is_file() or target.is_symlink():
+            if args.yes or prompt(origin, target, "remove"):
+                target.unlink()
+                return
 
+        elif target.is_dir():
+            shutil.rmtree(str(target))  # very scary
+            return
 
-def replace_in_subdirs(origin, target):
-    if origin.samefile(target):
-        return
-
-
-def prompt(origin, target, action=None):
-    if action == "remove":
-        text = """Do you wish to remove '%s'?
-{y(es) / n(o); Y(ES) (to all) / N(O) (to all)}: """ % str(target)
-    elif action == "replace":
-        text = """Do you wish to replace '%s' with '%s'?
-{y(es) / n(o); Y(ES) (to all) / N(O) (to all)}: """ % (str(origin), str(target))
     else:
-        text = """Do you wish to symlink '%s' to '%s'?
-{y(es) / n(o); Y(ES) (to all) / N(O) (to all)}: """ % (str(origin), str(target))
+        if target.exists():
+            if args.skip:
+                print("Skipping '%s', already exists..." % str(target))
+                return
+
+            if args.replace or args.yes or prompt(origin, target, "replace"):
+                if pathlib.Path.home() != pathlib.Path(*target.parts[:3]):
+                    print(warning_colour("Skipping '%s'. It is outside of home folder." %
+                                         str(target)))
+                    return
+
+                if target.is_file() or target.is_symlink():
+                    target.unlink()
+
+                elif target.is_dir():
+                    shutil.rmtree(str(target))  # very scary
+
+                target.symlink_to(origin, origin.is_dir())
+                return
+        else:
+            if args.yes or prompt(origin, target):
+                target.symlink_to(origin, origin.is_dir())
+
+
+def traverse_subdirs(origin):
+    global ignore
+    for subdir, dirs, files in os.walk(origin, topdown=True):
+        # https://stackoverflow.com/questions/19859840/excluding-directories-in-os-walk
+        [dirs.remove(d) for d in list(dirs) if d in ignore]
+        subdir = pathlib.Path(subdir)
+        target = target_path(subdir)
+
+        # Symlink folders if target is empty or already a symlink
+        if not target.exists():
+            symlink(subdir, target)
+            continue
+
+        if target.is_symlink():
+            symlink(subdir, target)
+            continue
+
+        if sum([1 for d in subdir.iterdir()]) == 0:  # if directory is empty
+            symlink(subdir, target)
+            continue
+
+        for f in files:
+            f = pathlib.Path(str(subdir) + "/" + f)
+            target = target_path(f)
+            symlink(f, target)
+            # if [f.stem == x.stem for x in (origin.glob("*.*"))]:  # if file exists
+
+
+def target_path(origin):
+    if "etc" in origin.parts:
+        target = "/" / pathlib.Path(*origin.parts[4:])
+    else:
+        target = pathlib.Path.home() / pathlib.Path(*origin.parts[5:])
+    return target
+
+
+def prompt(origin, target, action="symlink"):
+    colour = Colour()
+    text = colour.BOLD + action.capitalize() + colour.RESET
+    if action == "remove":
+        text += " '%s'?" % highlight_colour(str(target))
+    elif action == "replace":
+        text += " '%s' with '%s'?" % \
+                (highlight_colour(str(origin)), highlight_colour(str(target)))
+    else:
+        text += " '%s' to '%s'?" % \
+                (highlight_colour(str(origin)), highlight_colour(str(target)))
+    text += "\n[y(es) / n(o); Y(ES) (to all) / N(O) (to all)]: "
 
     if target.is_dir() and (action == "replace" or action == "remove"):
-        text = text.replace(": ", "") + "\nWARNING: This will delete all contents in the directory: "
+        text = text.replace(": ", "") + \
+                error_colour("\nWARNING: This will delete all contents in the directory: ")
     elif target.is_file() and (action == "replace" or action == "remove"):
-        text = text.replace(": ", "") + "\nWARNING: This will delete the file: "
+        text = text.replace(": ", "") + \
+                error_colour("\nWARNING: This will delete the target file: ")
 
     while True:
         inp = input(text)
@@ -125,41 +180,53 @@ def prompt(origin, target, action=None):
             return False
         elif inp.startswith("Y"):
             args.yes = True
-            args.verbose = True
-            if action == "replace":
-                args.replace = True
             return True
         elif inp.startswith("N"):
+            exit("Exiting.")
             args.no = True
-            args.verbose = True
-            if action == "replace":
-                args.skip = True
             return False
 
 
 def print_ln(origin, target):
-    if not args.remove:
-        print("ln -s %s %s" % (str(origin), str(target)))
-    else:
+    if args.remove:
         print("unlink %s" % str(target))
+    elif args.replace:
+        print("unlink %s" % str(target))
+        print("ln -s %s %s" % (str(origin), str(target)))
+
+    else:
+        print("ln -s %s %s" % (str(origin), str(target)))
+
+
+def warning_colour(s):
+    colour = Colour()
+    return colour.YELLOW + s + colour.RESET
+
+
+def error_colour(s):
+    colour = Colour()
+    return colour.RED + s + colour.RESET
+
+
+def highlight_colour(s):
+    colour = Colour()
+    return colour.CYAN + s + colour.RESET
+
 
 
 if __name__ == "__main__":
-    home = pathlib.Path.home()
-    dotfiles_dir = pathlib.Path(str(home) + "/dotfiles")
+    colour = Colour()
+    dotfiles_dir = pathlib.Path(pathlib.Path.home() / "dotfiles")
 
-    if not dotfiles_dir.exists():
-        exit("ERROR: '%s' is not a directory." % (str(home) + "/dotfiles"))
+    if not dotfiles_dir.is_dir():
+        exit(colour.RED + "ERROR: '%s' is not a directory."
+             % str(dotfiles_dir) + colour.RESET)
 
     for path in dotfiles_dir.iterdir():
-        if path.is_dir():
-            if path.stem in ignore:
-                continue
-            elif path.stem == "etc":
-                try:
-                    symlink(path, pathlib.Path("/etc"))
-                except PermissionError:
-                    exit("Permission denied. Please run the script as root if you want to symlink to '/etc'")
-            else:
-                sub_path(path, home)
+        if path.is_dir() and path.stem not in ignore:
+            try:
+                traverse_subdirs(path)
+            except PermissionError:
+                exit(error_colour("ERROR: Permission denied. Please run the script as root if you want to symlink outside of home folder"))
+
 
