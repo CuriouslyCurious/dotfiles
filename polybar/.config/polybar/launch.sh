ebdbc63cf48949874e5d7016d8168db6c1776edc
CuriouslyCurious <ojaloe-5@student.ltu.se>
2018-01-02 21:42:17 +0100

    polybar: more general launch.sh

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env sh
+#!/bin/sh
+# https://github.com/jaagr/polybar/issues/555
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,11 +7,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-#polybar top &
-#polybar top-right &
+# Launch bar1 and bar2
+DISPLAY1="$(xrandr -q | grep 'LVDS1\|eDP1\|VGA-1' | cut -d ' ' -f1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar main &
 
-MONITOR=LVDS1 polybar main &
-MONITOR=HDMI-1 polybar secondary &
-MONITOR=DVI-D-1 polybar tertiary &
+DISPLAY2="$(xrandr -q | grep 'HDMI1\|DVI-I-1' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar secondary &
 
 echo "Bars launched..."
+
