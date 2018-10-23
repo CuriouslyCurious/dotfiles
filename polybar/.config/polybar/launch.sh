b22629aca48820fbb5148f4c2e79df70d4f489e0
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2018-10-24 01:44:21 +0200

    Add keyboard switching and lang indicator

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,13 +8,13 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-DISPLAY1="$(xrandr -q | grep 'DP-1\|LVDS1\|VGA-1' | cut -d ' ' -f1 | head -n 1)"
-[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar main &
+DISPLAY1="$(xrandr -q | grep 'DisplayPort-0\|DP-1\|LVDS1\|VGA-1' | cut -d ' ' -f1 | head -n 1)"
+[[ ! -z "$DISPLAY1" ]] && MONITOR="$DISPLAY1" polybar main & 2>/dev/null
 
-DISPLAY2="$(xrandr -q | grep 'HDMI-1\|DVI-I-1' | cut -d ' ' -f1)"
-[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar main &
+DISPLAY2="$(xrandr -q | grep 'HDMI-0\|HDMI-1\|DVI-I-1' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY2 ]] && MONITOR=$DISPLAY2 polybar main & 2>/dev/null
 
-DISPLA3="$(xrandr -q | grep 'DVI-D-1' | cut -d ' ' -f1)"
-[[ ! -z $DISPLAY3 ]] && MONITOR=$DISPLAY3 polybar main &
+DISPLA3="$(xrandr -q | grep 'DVI-0\|DVI-D-1' | cut -d ' ' -f1)"
+[[ ! -z $DISPLAY3 ]] && MONITOR=$DISPLAY3 polybar main & 2>/dev/null
 echo "Bars launched..."
 
