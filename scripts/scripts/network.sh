09a0b169217faa35dd2344ace7cda4f16e000627
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2019-03-12 14:17:28 +0100

    scripts: Remove sudo from network.sh

--- scripts/scripts/network.sh
+++ scripts/scripts/network.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 INTERFACE="wlp3s0"
-sudo wpa_supplicant -B -i $INTERFACE -c /etc/wpa_supplicant/wpa_supplicant.conf
-sudo dhcpcd $INTERFACE
+wpa_supplicant -B -i $INTERFACE -c /etc/wpa_supplicant/wpa_supplicant.conf
+dhcpcd $INTERFACE
