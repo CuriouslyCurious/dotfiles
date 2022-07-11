40f65ccf8771828d8f4f662c2cc5fcc4c734dac8
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2022-07-11 21:50:56 +0200

    scripts: Add ssh-agent.sh script

---
+++ scripts/scripts/ssh-agent.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Start ssh agent
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent -t 1h > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" > /dev/null 2>&1
+fi
+
