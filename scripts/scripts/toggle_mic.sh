fe28175dec013a7d55aa3d2d8b87e724988b00b0
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2022-07-13 23:00:13 +0200

    scripts: Change toggle_mic.sh to use get-default-source

--- scripts/scripts/toggle_mic.sh
+++ scripts/scripts/toggle_mic.sh
@@ -4,7 +4,7 @@
 sound_effect="$HOME/Downloads/beep.wav"
 
 # Get current default source
-default_source="$(pactl list sources short | grep input | awk '{print $1}')"
+default_source="$(pactl get-default-source)"
 
 # Toggle mute status
 pactl set-source-mute $default_source toggle
