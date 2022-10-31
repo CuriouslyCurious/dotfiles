860b8c16e836f16f3bbba9e888cc5f6140d6f5f0
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2022-10-31 13:04:08 +0100

    fish: Remove envs from fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,53 +1,7 @@
 # Enable vi mode
 fish_vi_key_bindings
 
-# Set default editor
-if which nvim > /dev/null 2>&1
-	set -U EDITOR nvim
-else if which vim > /dev/null 2>&1
-	set -U EDITOR vim
-else
-	set -U EDITOR nano
-end
-
-# Run ssh-agent
-# https://wiki.archlinux.org/index.php/SSH_keys#SSH_agents
-#if ! pgrep -u "$USER" ssh-agent > /dev/null
-#    ssh-agent -c -t 1h > "$HOME/.ssh/ssh-agent.env"
-#end
-#if test -e "$SSH_AUTH_SOCK"
-#    eval "$HOME/.ssh/ssh-agent.env" > /dev/null
-#end
-
 set GPG_TTY (tty)
-
-set PATH "$HOME/.local/bin:$PATH"
-
-# Export flatpaks to $PATH
-if test -e $HOME/.local/share/flatpak/app
-    set PATH "$HOME/.local/share/flatpak/app:$PATH"
-end
-
-# Export rustup to $PATH if it exists
-if test -e $HOME/.cargo/bin
-    set PATH "$HOME/.cargo/bin:$PATH"
-end
-
-# Export go/bin to $GOPATH if it exists and then set -U to $PATH
-if test -d $HOME/go/bin
-    set GOPATH "$HOME/go/bin"
-    set PATH "$GOPATH:$PATH"
-end
-
-# Export gem path to $PATH if it exists
-if test -d "$HOME/.gem/ruby/2.5.0/bin"
-    set PATH "$HOME/.gem/ruby/2.5.0/bin:$PATH"
-end
-
-# Export dotfile's script dir to $PATH if it exists
-if test -d "$HOME/scripts"
-    set PATH "$HOME/scripts:$PATH"
-end
 
 # Ctrl-backspace working as it should
 #bind \e\[3\;5~ kill-word
@@ -58,15 +12,11 @@ bind -M insert \cH backward-kill-word
 bind H beginning-of-line
 bind L end-of-line
 
-# Load dircolors
-# eval (gdircolors ~/.dircolors)
-
 # Load zoxide
 zoxide init --cmd cd fish | source
 
 # Load prompt
 starship init fish | source
-
 
 # Autostart X at login
 #if status is-login
